fe5cf06f63361ead41e4bbaa9d33c8593765dc05
Konpat Preechakul <the.akita.ta@gmail.com>
2018-07-25 00:43:50 +0700

    update jupyter

--- tensorflow/image/usr/local/bin/jupyter.sh
+++ tensorflow/image/usr/local/bin/jupyter.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
-# create a default password if there is no jupyter config
-if [ ! -d ${HOME}/.jupyter ]; then
-    echo "creating a simple password"
-    jupyter notebook --generate-config
-    python -c "from notebook.auth import passwd; print(passwd('$JUPYTER_PASSWORD'))" | xargs printf "c.NotebookApp.password = u'%s'\n" >> /root/.jupyter/jupyter_notebook_config.py 
-fi
-
-jupyter lab --ip=* --port=8888 --no-browser --allow-root ${HOME}
+jupyter lab \
+    --ip=* --port=8888 \
+    --no-browser \
+    --allow-root \
+    --NotebookApp.token="${JUPYTER_TOKEN}" \
+    --NotebookApp.password="${JUPYTER_PASSWORD}" \
+    ${HOME}
