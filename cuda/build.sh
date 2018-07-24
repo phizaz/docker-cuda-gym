9b51f1a0933443d6783629fe8e2128f49a404d6d
Konpat Preechakul <the.akita.ta@gmail.com>
2018-07-25 01:08:55 +0700

    build with --squash to minimze the image size

--- cuda/build.sh
+++ cuda/build.sh
@@ -1,1 +1,5 @@
-docker build -t ta/cuda:9.0-ubuntu18.04 .
+# build using squash (this will make the resulting image much smaller)
+docker build --squash -t ta/cuda:9.0-ubuntu18.04 .
+
+# original build 
+# docker build -t ta/cuda:9.0-ubuntu18.04 .
